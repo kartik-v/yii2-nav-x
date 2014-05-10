f4866950ef29224087284a98f423138318998374
Kartik Visweswaran <kartikv2@gmail.com>
2014-05-10 11:45:06 +0530

    Initial release

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
 yii2-nav-x
 ==========
 
-Extended bootstrap Nav widget with submenu option for Yii 2.
+An extended bootstrap Nav widget for Yii Framework 2 with submenu drilldown. This widget extends the `\yii\bootstrap\Nav` widget
+with some additional controls and adding the CSS Styles for enabling a submenu drilldown.
+
+> NOTE: This extension depends on the [yiisoft/yii2-bootstrap](https://github.com/yiisoft/yii2/tree/master/extensions/bootstrap) extension. Check the 
+[composer.json](https://github.com/kartik-v/yii2-builder/blob/master/composer.json) for this extension's requirements and dependencies. 
+Note: Yii 2 framework is still in active development, and until a fully stable Yii2 release, your core yii2-bootstrap packages (and its dependencies) 
+may be updated when you install or update this extension. You may need to lock your composer package versions for your specific app, and test 
+for extension break if you do not wish to auto update dependencies.
+
+### Demo
+You can see detailed [documentation](http://demos.krajee.com/nav-x) on usage of the extension.
+
+## Installation
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+$ php composer.phar require kartik-v/yii2-nav-x "dev-master"
+```
+
+or add
+
+```
+"kartik-v/yii2-nav-x": "dev-master"
+```
+
+to the ```require``` section of your `composer.json` file.
+
+## Usage
+
+### NavX
+
+```php
+use kartik\nav\NavX;
+echo NavX::widget([
+    'options' => ['class' => 'navbar-nav'],
+    'items' => [
+        ['label' => 'Action', 'url' => '#'],
+        ['label' => 'Submenu', 'items' => [
+            ['label' => 'Action', 'url' => '#'],
+            ['label' => 'Another action', 'url' => '#'],
+            ['label' => 'Something else here', 'url' => '#'],
+        ]],
+        ['label' => 'Something else here', 'url' => '#'],
+        '<li class="divider"></li>',
+        ['label' => 'Separated link', 'url' => '#'],
+    ],
+    'encodeLabels' => false
+]);
+```
+
+## License
+
+**yii2-nav-x** is released under the BSD 3-Clause License. See the bundled `LICENSE.md` for details.
---
+++ nav/NavX.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014
+ * @package yii2-nav-x
+ * @version 1.0.0
+ */
+
+namespace kartik\nav;
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+
+/**
+ * An extended nav menu for Bootstrap 3 - that offers
+ * submenu drilldown
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+class NavX extends \yii\bootstrap\Nav
+{
+    /**
+     * @var string the class name to render the Dropdown items.
+     * Defaults to `\kartik\dropdown\DropdownX`.
+     */
+    public $dropdownClass = '\kartik\dropdown\DropdownX';
+    
+    /**
+     * @var array the dropdown widget options
+     */
+    public $dropdownOptions = [];
+    
+    /**
+     * @var string the caret indicator to display for dropdowns
+     */
+    public $dropdownIndicator = ' <span class="caret"></span>';
+    
+    /**
+     * Initialize the widget
+     * @throws InvalidConfigException
+     */
+    public function init() {
+        if (!class_exists($this->dropdownClass)) {
+             throw new InvalidConfigException("The dropdownClass '{$this->dropdownClass}' does not exist or is not accessible.");
+        }
+        parent::init();
+    }
+    
+    /**
+     * Renders a widget's item.
+     * @param string|array $item the item to render.
+     * @return string the rendering result.
+     * @throws InvalidConfigException
+     */
+    public function renderItem($item)
+    {
+        if (is_string($item)) {
+            return $item;
+        }
+        if (!isset($item['label'])) {
+            throw new InvalidConfigException("The 'label' option is required.");
+        }
+        $label = $this->encodeLabels ? Html::encode($item['label']) : $item['label'];
+        $options = ArrayHelper::getValue($item, 'options', []);
+        $items = ArrayHelper::getValue($item, 'items');
+        $url = ArrayHelper::getValue($item, 'url', '#');
+        $linkOptions = ArrayHelper::getValue($item, 'linkOptions', []);
+
+        if (isset($item['active'])) {
+            $active = ArrayHelper::remove($item, 'active', false);
+        } else {
+            $active = $this->isItemActive($item);
+        }
+
+        if ($items !== null) {
+            $linkOptions['data-toggle'] = 'dropdown';
+            Html::addCssClass($options, 'dropdown');
+            Html::addCssClass($linkOptions, 'dropdown-toggle');
+            $label .= $this->dropdownIndicator;
+            if (is_array($items)) {
+                if ($this->activateItems) {
+                    $items = $this->isChildActive($items, $active);
+                }
+                $dropdown = $this->dropdownClass;
+                $items = $dropdown::widget([
+                    'items' => $items,
+                    'encodeLabels' => $this->encodeLabels,
+                    'clientOptions' => false,
+                    'view' => $this->getView(),
+                ] + $this->dropdownOptions);
+            }
+        }
+
+        if ($this->activateItems && $active) {
+            Html::addCssClass($options, 'active');
+        }
+
+        return Html::tag('li', Html::a($label, $url, $linkOptions) . $items, $options);
+    }
+}
